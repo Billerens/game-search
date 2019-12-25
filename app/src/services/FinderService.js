c02d59281ea6cf6c6b3e9aef19d5c61039d12471
Billerens <degtyarik.ilya@yandex.ru>
2019-12-25 20:50:44 +0300

    App clearing. MARKUP!

--- app/src/services/FinderService.js
+++ app/src/services/FinderService.js
@@ -3,7 +3,7 @@ import api from "../helpers/api";
 class FinderService {
   async findAll(query) {
     console.log(query);
-    var finded = await api("/api/findGame", "POST", query);
+    var finded = await api("/findGame", "POST", query);
     return JSON.parse(finded.data);
   }
 }
