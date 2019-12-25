47609dab2032ac3217b33182965a310395d994c9
Billerens <degtyarik.ilya@yandex.ru>
2019-12-25 19:00:02 +0300

    Restored

--- server/app.js
+++ server/app.js
@@ -2,18 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const Controllers = require("./controllers");
-const favicon = require("express-favicon");
-const path = require("path");
 
 const app = express();
-app.use(favicon(__dirname + "/build/favicon.ico"));
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
 app.use(bodyParser.json({ limit: "10mb", extended: true }));
 app.use(cors());
 
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get("/", function(req, res) {
+  res.send("I'm here!!!");
 });
 
 app.use("/api/findGame", Controllers.RAWGFinder);
